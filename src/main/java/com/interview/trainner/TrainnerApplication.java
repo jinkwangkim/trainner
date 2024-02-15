5374272313ffcb2ed0fc57def12a5d8bb367b9f2
kwang23 <kwang23@gmail.com>
2024-02-15 14:11:36 +0900

    프로젝트 초기화

---
+++ src/main/java/com/interview/trainner/TrainnerApplication.java
@@ -0,0 +1,13 @@
+package com.interview.trainner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/interview/trainner/TrainnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.trainner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
