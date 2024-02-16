0f65f9ba2d2e6ea0a8faa83e7e1925db4d028913
kwang23 <kwang23@gmail.com>
2024-02-16 17:23:23 +0900

    프로젝트 초기화

--- src/main/java/com/interview/trainner/HelloController.java
+++ src/main/java/com/interview/trainner/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/api/hello")
+    @GetMapping("/api/hello")dddd
     public String hello() {
         return "hello";
     }
