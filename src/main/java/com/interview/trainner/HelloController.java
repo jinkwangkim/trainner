734406d87a360ec7f1506e84e89fd2d4b3db81ef
areumyang <yarm76@naver.com>
2024-02-17 10:35:55 +0900

    template 엔진 추가 (thymeleaf)

--- src/main/java/com/interview/trainner/HelloController.java
+++ src/main/java/com/interview/trainner/HelloController.java
@@ -1,12 +1,19 @@
 package com.interview.trainner;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/api/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
     }
 }
